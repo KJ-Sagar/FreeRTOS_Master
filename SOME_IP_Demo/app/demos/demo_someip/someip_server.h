90ee5868bada0fa9dd85acf8076dd86838b85753
KJ-Sagar <kjagannathsagar@gmail.com>
2026-01-20 11:46:20 +0530

    Delete SOME_IP_Demo directory from the root

--- SOME_IP_Demo/app/demos/demo_someip/someip_server.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void someip_server_start(void);
--- SOME_IP_Demo/build/app/demos/demo_someip/demo_someip.d
+++
@@ -1,3 +0,0 @@
-build/app/demos/demo_someip/demo_someip.o: \
- app/demos/demo_someip/demo_someip.c app/demos/demo_someip/demo_someip.h \
- app/demos/demo_someip/someip_server.h
